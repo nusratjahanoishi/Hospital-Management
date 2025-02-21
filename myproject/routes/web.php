84387f2b5748e7b8a6d3a00b11f873ecf99d4167
Radoan Asif <121860219+radoan114@users.noreply.github.com>
2025-02-21 22:43:23 +0600

    Fix landing page navigation and layout

--- myproject/routes/web.php
+++ myproject/routes/web.php
@@ -19,9 +19,10 @@ use App\Http\Controllers\PatientController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// landing page update problemn
 
 Route::get('/', function () {
-    return view('layout.auth.register');
+    return view('landing'); // Change this line to point to your landing page view
 });
 
 // Registration routes
@@ -38,15 +39,11 @@ Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::get('/patient/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-
 Route::get('/patient/doctor/index', [PatientController::class, 'index'])->name('patient.doctor.index');
 Route::get('/patient/appointment/list{id}', [PatientController::class, 'list'])->name('patient.appointment.list');
 Route::get('/patient/doctor/profile{id}', [PatientController::class, 'doctor_profile'])->name('patient.doctor.profile');
 Route::get('/patient/doctor/appointment{id}', [PatientController::class, 'appointment'])->name('patient.doctor.appointment');
 Route::post('/patient/appointment/store', [PatientController::class, 'store'])->name('patient.appointment.store');
-// Route::post('/doctor/store', [DoctorController::class, 'store'])->name('doctor.store');
-// Route::get('/doctor/edit/{id}', [DoctorController::class, 'edit'])->name('doctor.edit');
-// Route::get('/doctor/delete/{id}', [DoctorController::class, 'delete'])->name('doctor.delete');
 
 //Doctor
 Route::get('/doctor/register', function() {
@@ -69,9 +66,6 @@ Route::post('/doctor/store', [DoctorController::class, 'store'])->name('doctor.s
 Route::get('/doctor/edit/{id}', [DoctorController::class, 'edit'])->name('doctor.edit');
 Route::get('/doctor/delete/{id}', [DoctorController::class, 'delete'])->name('doctor.delete');
 
-
-
-
 //Admin
 Route::get('/admin/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
 
@@ -87,14 +81,11 @@ Route::post('/admin/position/store', [DoctorExpertController::class, 'store'])->
 Route::get('/admin/position/edit/{id}', [DoctorExpertController::class, 'edit'])->name('doctor.position.edit');
 Route::get('/admin/position/delete/{id}', [DoctorExpertController::class, 'delete'])->name('doctor.position.delete');
 
-
-
-
 //Appointment
 Route::get('/appointments', [AppointmentController::class, 'index'])->name('appointments.index');
-    Route::get('/appointments/create', [AppointmentController::class, 'create'])->name('appointments.create');
-    Route::post('/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
-    Route::post('/appointments/status', [AppointmentController::class, 'status'])->name('appointments.status');
-    Route::get('/appointments/{appointment}', [AppointmentController::class, 'show'])->name('appointments.show');
-    Route::put('/appointments/{appointment}', [AppointmentController::class, 'update'])->name('appointments.update');
-    Route::get('/appointments/delete/{appointment}', [AppointmentController::class, 'destroy'])->name('appointments.delete'); 
+Route::get('/appointments/create', [AppointmentController::class, 'create'])->name('appointments.create');
+Route::post('/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
+Route::post('/appointments/status', [AppointmentController::class, 'status'])->name('appointments.status');
+Route::get('/appointments/{appointment}', [AppointmentController::class, 'show'])->name('appointments.show');
+Route::put('/appointments/{appointment}', [AppointmentController::class, 'update'])->name('appointments.update');
+Route::get('/appointments/delete/{appointment}', [AppointmentController::class, 'destroy'])->name('appointments.delete');
